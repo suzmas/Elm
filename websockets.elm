904a2e53aa6a79aee205cb4bf782c87c6df1b9b5
Suzan Sucro <suzansucro@gmail.com>
2017-05-07 22:44:15 -0700

    k New message.

---
+++ websockets.elm
@@ -0,0 +1,68 @@
+-- Read more about this program in the official Elm guide:
+-- https://guide.elm-lang.org/architecture/effects/random.html
+
+
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Events exposing (..)
+import Random
+
+
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { dieFace : Int
+    }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Roll
+    | NewFace Int
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Roll ->
+            ( model, Cmd.none )
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h1 [] [ text (toString model.dieFace) ]
+        , button [ onClick Roll ] [ text "Roll" ]
+        ]
+
+
+
+-- SUBSCRIPTIONS
+-- subscriptions : Model -> Sub Msg
+-- subscriptions model =
+--     model
+-- INIT
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( Model 1, Cmd.none )
